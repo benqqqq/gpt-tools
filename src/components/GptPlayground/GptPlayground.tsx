c8db8a8c264c78dcf9955ba3629d04be9e3988a5
mitchwu <benq7879@gmail.com>
2024-08-10 19:05:53 +0800

    chores: add gpt-4o and gpt-4o-mini support

--- src/components/GptPlayground/GptPlayground.tsx
+++ src/components/GptPlayground/GptPlayground.tsx
@@ -64,7 +64,7 @@ export default function GptPlayground(): ReactElement {
 	const navigate = useNavigate()
 	const [gptTemperature, setGptTemperature] = useState(GPT_TEMPERATURE)
 	const [gptModel, setGptModel] =
-		useState<IChatCompletionOptions['model']>('gpt-3.5-turbo')
+		useState<IChatCompletionOptions['model']>('gpt-4o-mini')
 	const messageRef = useRef<HTMLDivElement>(null)
 
 	const submitMessages = useCallback(
--- src/services/OpenAiContext.tsx
+++ src/services/OpenAiContext.tsx
@@ -13,7 +13,9 @@ export const GPT_MODELS = [
 	'gpt-3.5-turbo-16k',
 	'gpt-3.5-turbo',
 	'gpt-4',
-	'gpt-4-32k'
+	'gpt-4-32k',
+	'gpt-4o',
+	'gpt-4o-mini'
 ] as const
 
 export interface IChatCompletionOptions {
